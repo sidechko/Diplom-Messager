8e8e6ba09d89851406ffcdbc878bc399ccad2603
S1dechko <slaaaadd@mail.ru>
2023-06-01 07:30:11 +0700

    edit 2

--- MessagerClient/MessagerClient/obj/Debug/net6.0-windows/AppendUserToChannel.g.i.cs
+++ MessagerClient/MessagerClient/obj/Debug/net6.0-windows/AppendUserToChannel.g.i.cs
@@ -61,7 +61,7 @@ namespace MessagerClient {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MessagerClient;component/appendusertochannel.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MessagerClient;V1.0.0.0;component/appendusertochannel.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\AppendUserToChannel.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- MessagerClient/MessagerClient/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ MessagerClient/MessagerClient/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -110,7 +110,7 @@ namespace MessagerClient {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MessagerClient;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MessagerClient;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
